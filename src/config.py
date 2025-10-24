72ef5db5d0306fde9e52f9009246c2311b671afd
angeloken <58063425+angeloken@users.noreply.github.com>
2025-10-24 15:39:41 +1100

    updated

--- src/config.py
+++ src/config.py
@@ -1,6 +1,5 @@
 import os
-HF_API_KEY = "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"
-MODEL = "bigscience/bloomz-3b" 
+
 #OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "sk-yourkey")
 #MODEL = "gpt-3.5-turbo"
 CATEGORIES = ["Cleanser", "Toner", "Essence", "Serum", "Moisturizer",
